cb41589f92d7a768695005e20ce8bd0a45977e66
Mohamed Badry <m.badry.fl@gmail.com>
2026-07-21 21:45:58 +0300

    Fix Noctalia read-only file system error by using mutable copies

--- modules/home/desktop/noctalia.nix
+++ modules/home/desktop/noctalia.nix
@@ -2,9 +2,12 @@
 {
   home.packages = [ pkgs.noctalia-shell ];
 
-  xdg.configFile = {
-    "noctalia/colors.json".source = ../../../config/noctalia/colors.json;
-    "noctalia/plugins.json".source = ../../../config/noctalia/plugins.json;
-    "noctalia/settings.json".source = ../../../config/noctalia/settings.json;
-  };
+  home.activation.copyNoctaliaConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p $HOME/.config/noctalia
+    rm -f $HOME/.config/noctalia/colors.json $HOME/.config/noctalia/plugins.json $HOME/.config/noctalia/settings.json
+    cp ${../../../config/noctalia/colors.json} $HOME/.config/noctalia/colors.json
+    cp ${../../../config/noctalia/plugins.json} $HOME/.config/noctalia/plugins.json
+    cp ${../../../config/noctalia/settings.json} $HOME/.config/noctalia/settings.json
+    chmod u+w $HOME/.config/noctalia/*.json
+  '';
 }
