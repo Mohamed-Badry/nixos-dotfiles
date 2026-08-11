c1406834ac414d05df22465fdec139ae2ab3a56f
Mohamed Badry <m.badry.fl@gmail.com>
2026-08-11 04:38:17 +0300

    changed nix settings to keep derivations and outputs so they're not cleared by nix-collect-garbage

--- modules/nixos/base/default.nix
+++ modules/nixos/base/default.nix
@@ -15,6 +15,8 @@
 
   nixpkgs.config.allowUnfree = true;
   nix.settings = {
+    keep-outputs = true;
+    keep-derivations = true;
     experimental-features = [
       "nix-command"
       "flakes"
